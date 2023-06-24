55c80a54f928e6bd4cf46ced4dc7e03fd59c65ae
Jean Folle Vanz <jeanfolle86@gmail.com>
2023-06-24 14:06:08 -0300

    Ajustado o tamanho da fonte geral de 20 para 21. Adicionados e fixados na tela 'pressF10', 'pressF11' e 'pressF12', porém até o momento apenas o F12 é funcional. Também otimizada a função do clique, removendo lists desnecessárias e unificando tudo na list 'estrelas'

--- main.py
+++ main.py
@@ -13,27 +13,30 @@ pygame.display.set_caption('Space Marker')
 pygame.mixer.music.load('soundtrack.mp3')
 pygame.mixer.music.play(-1)
 estrela=pygame.image.load('star.png')
-fonte=pygame.font.Font(None,20)
+fonte=pygame.font.Font(None,21)
+pressF10=fonte.render('Pressione F10 para Salvar os Pontos',True,branco)
+pressF11=fonte.render('Pressione F11 para Carregar os Pontos',True,branco)
+pressF12=fonte.render('Pressione F12 para Deletar os Pontos',True,branco)
 running=True
 estrelas=[]
-nomeestrelas=[]
-coordenadas=[]
 while running:
     for evento in pygame.event.get():
         if evento.type==pygame.QUIT:
             quit()
         elif evento.type==pygame.MOUSEBUTTONUP:
             posicao=pygame.mouse.get_pos()
-            estrelas.append(posicao)
             nome=simpledialog.askstring('Space','Nomeie o ponto selecionado:')
-            nomeestrelas.append(nome)
-            if nome == '':
+            if nome == None:
                 nome='desconhecido'+str(posicao)
-            coordenadas.append((nome,posicao))
-            print(nomeestrelas)
-            print(posicao)
+            estrelas.append((nome,posicao))
+            print(estrelas)
+        elif evento.type==pygame.KEYDOWN and evento.key==pygame.K_F12:
+            estrelas=[]
     tela.blit(fundo,(0,0))
-    for nome, posicao in coordenadas:
+    tela.blit(pressF10,(10,20))
+    tela.blit(pressF11,(10,40))
+    tela.blit(pressF12,(10,60))
+    for nome, posicao in estrelas:
         posicaoestrela=(posicao[0]-estrela.get_width()//2,posicao[1]-estrela.get_height()//2)
         tela.blit(estrela,posicaoestrela)
         textoestrela=fonte.render(nome,True,branco)

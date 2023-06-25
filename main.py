27822b746117a12dc182579ae18e6890a0df1438
Jean Folle Vanz <jeanfolle86@gmail.com>
2023-06-25 16:37:50 -0300

    Adicionadas novas mensagens que aparecem na tela por 3 segundos caso já exista um ponto cadastrado com certo nome e tente informar outro ponto com a mesma nomenclatura. Outra mensagem para quando pressionar f10 informe que os pontos foram salvos. Ou assim que pressionar f11, dependendo das circuntâncias, apareça que não há pontos para carregar, ou que houve erro ao carregar os pontos(possívelmente há uma forma mais otimizada de replicar a geração dessas mensagens, mas por enquanto irei deixar desta forma pois está funcional)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 from tkinter import simpledialog
-import math 
-import pickle
+import math
 import json
 pygame.init()
 tamanho=(1280,720)
@@ -21,38 +20,79 @@ pressF10=fonte.render('Pressione F10 para Salvar os Pontos',True,branco)
 pressF11=fonte.render('Pressione F11 para Carregar os Pontos',True,branco)
 pressF12=fonte.render('Pressione F12 para Deletar os Pontos',True,branco)
 running=True
-estrelas={}
+mensagemexibida=False
+mensagemtempoinicial=0
+mensagemduracao=3000
 def calculardistancia(ponto1, ponto2): 
-    return math.sqrt((ponto2[0] - ponto1[0])**2+(ponto2[1] - ponto1[1])**2)    
+    return math.sqrt((ponto2[0] - ponto1[0])**2+(ponto2[1] - ponto1[1])**2)
+estrelas={}
 while running:
     for evento in pygame.event.get():
-        if evento.type==pygame.QUIT:
+        if evento.type==pygame.QUIT or evento.type==pygame.KEYDOWN and evento.key==pygame.K_ESCAPE:
             running=False
-        elif evento.type==pygame.KEYDOWN and evento.key==pygame.K_ESCAPE:
-            pygame.quit()
         elif evento.type==pygame.MOUSEBUTTONUP:
             posicao=pygame.mouse.get_pos()
             nome=simpledialog.askstring('Space','Nomeie o ponto selecionado:')
             if nome == None or nome == '':
                 nome='desconhecido'+str(posicao)
-            estrelas[nome] = posicao
-            print(estrelas)
+            elif nome in estrelas:
+                mensagem=fonte.render(f'Já existe um ponto cadastrado com esse nome: {nome}', True, branco)
+                mensagemlargura=mensagem.get_width()
+                mensagemaltura=mensagem.get_height()
+                mensagemposicao=(tamanho[0] // 2 - mensagemlargura // 2, tamanho[1] // 2 - mensagemaltura // 2)
+                mensagemexibida=True
+                mensagemtempoinicial=pygame.time.get_ticks()
+                continue
+            estrelas[nome]=posicao
         elif evento.type==pygame.KEYDOWN and evento.key==pygame.K_F12:
             estrelas={}
+        elif evento.type==pygame.KEYDOWN and evento.key==pygame.K_F11:
+            try:
+                with open('db.txt') as database:
+                    estrelas=json.load(database)
+                if not estrelas:
+                    mensagem=fonte.render(f'Não há pontos salvos para carregar', True, branco)
+                    mensagemlargura=mensagem.get_width()
+                    mensagemaltura=mensagem.get_height()
+                    mensagemposicao=(tamanho[0] // 2 - mensagemlargura // 2, tamanho[1] // 2 - mensagemaltura // 2)
+                    mensagemexibida=True
+                    mensagemtempoinicial=pygame.time.get_ticks()
+                    continue
+            except FileNotFoundError:
+                mensagem=fonte.render(f'Não há pontos salvos para carregar', True, branco)
+                mensagemlargura=mensagem.get_width()
+                mensagemaltura=mensagem.get_height()
+                mensagemposicao=(tamanho[0] // 2 - mensagemlargura // 2, tamanho[1] // 2 - mensagemaltura // 2)
+                mensagemexibida=True
+                mensagemtempoinicial=pygame.time.get_ticks()
+                continue
+            except json.decoder.JSONDecodeError:
+                mensagem=fonte.render(f'Erro ao carregar os pontos', True, branco)
+                mensagemlargura=mensagem.get_width()
+                mensagemaltura=mensagem.get_height()
+                mensagemposicao=(tamanho[0] // 2 - mensagemlargura // 2, tamanho[1] // 2 - mensagemaltura // 2)
+                mensagemexibida=True
+                mensagemtempoinicial=pygame.time.get_ticks()
+                continue
         elif evento.type==pygame.KEYDOWN and evento.key==pygame.K_F10:
             try:
                 with open('db.txt','w') as database:
                     json.dump(estrelas,database)
+                    mensagem=fonte.render(f'Pontos salvos', True, branco)
+                    mensagemlargura=mensagem.get_width()
+                    mensagemaltura=mensagem.get_height()
+                    mensagemposicao=(tamanho[0] // 2 - mensagemlargura // 2, tamanho[1] // 2 - mensagemaltura // 2)
+                    mensagemexibida=True
+                    mensagemtempoinicial=pygame.time.get_ticks()
+                    continue
             except:
-                print("Erro ao criar base de dados")
-
-        elif evento.type==pygame.KEYDOWN and evento.key==pygame.K_F11:
-            try:
-                with open('db.txt') as database:
-                    estrelas = json.load(database)
-            except:
-                print("Erro ao salvar os pontos")
-
+                mensagem=fonte.render(f'Erro ao criar base de dados', True, branco)
+                mensagemlargura=mensagem.get_width()
+                mensagemaltura=mensagem.get_height()
+                mensagemposicao=(tamanho[0] // 2 - mensagemlargura // 2, tamanho[1] // 2 - mensagemaltura // 2)
+                mensagemexibida=True
+                mensagemtempoinicial=pygame.time.get_ticks()
+                continue
     tela.blit(fundo,(0,0))
     tela.blit(pressF10,(10,20))
     tela.blit(pressF11,(10,40))
@@ -66,16 +106,20 @@ while running:
             pontos = list(estrelas.values())
             pygame.draw.lines(tela, branco, False, pontos, 1)
             for i in range(len(pontos) - 1):
-                pontoatual = pontos[i]
-                proximoponto = pontos[i + 1]
-                distancia = calculardistancia(pontoatual, proximoponto)
+                pontoatual=pontos[i]
+                proximoponto=pontos[i + 1]
+                distancia=calculardistancia(pontoatual, proximoponto)
                 distanciax=abs(proximoponto[0]-pontoatual[0])
                 distanciay=abs(proximoponto[1] - pontoatual[1])
-                textodistanciay = fonte.render(f"y: {distanciay:.2f}", True, branco)
-                textodistancia = fonte.render(f"x: {distanciax:.2f}", True, branco)
-                posicaotextodistancia = ((pontoatual[0] + proximoponto[0]) // 2 - 40, (pontoatual[1] + proximoponto[1]) // 2 - 20)
-                tela.blit(textodistancia, (posicaotextodistancia[0], posicaotextodistancia[1] - 40))
-                tela.blit(textodistanciay, (posicaotextodistancia[0], posicaotextodistancia[1] - 20))
+                textodistanciay=fonte.render(f"y: {distanciay:.2f}", True, branco)
+                textodistancia=fonte.render(f"x: {distanciax:.2f}", True, branco)
+                posicaotextodistancia=((pontoatual[0] + proximoponto[0]) // 2 - 40, (pontoatual[1] + proximoponto[1]) // 2 - 20)
+                tela.blit(textodistancia,(posicaotextodistancia[0],posicaotextodistancia[1]-40))
+                tela.blit(textodistanciay,(posicaotextodistancia[0],posicaotextodistancia[1]-20))
+    if mensagemexibida:
+        tela.blit(mensagem, mensagemposicao)
+        if pygame.time.get_ticks()-mensagemtempoinicial>mensagemduracao:
+            mensagemexibida=False
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
